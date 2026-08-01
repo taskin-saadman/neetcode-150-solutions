961ffc55e233414b32c163669729ae7006adb286
Taskin Saadman <taskin13882@gmail.com>
2026-08-02 08:12:40 +1000

    Solved Two Sum

--- 01_arrays_and_hashing/two_sum.py
+++ 01_arrays_and_hashing/two_sum.py
@@ -1,10 +1,30 @@
 """
 Two Sum
 
+the brute force solution would have used a nested loop
+and compare sum of every element-pair to the target.
+Although a correct solution, it would be O(n^2).
+
+We will use dictionary to obtain O(n) solution.
+
+
 Time Complexity:  O()
 Space Complexity: O()
 """
 
-
 class Solution:
-    pass
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen: dict = {} #num -> index mapping
+        for idx, num in nums:
+            difference = target - num
+            if difference in seen:
+                return [seen[difference],idx]
+            seen[num] = idx #else, update dict with idx value
+            '''
+            in hope of if in future difference potentially would exist in the dict
+            ,save curr values idx into the dictionary
+            COMPLEXITIES
+            this is O(n) time and O(n) space
+            dict get and put methods are O(1) expected time
+            space is O(n) since in worst case target sum is not found
+            '''
--- 01_arrays_and_hashing/valid_anagram.py
+++ 01_arrays_and_hashing/valid_anagram.py
@@ -1,10 +1,54 @@
 """
 Valid Anagram
 
-Time Complexity:  O()
-Space Complexity: O()
+Time Complexity:  O(m+n), m and n are lengths of s and t
+since we need 1 single pass through each of em
+    Space Complexity: O(m+n) for the 2 count dictionaries
 """
 
-
 class Solution:
-    pass
+    def isAnagram(self, s: str, t: str) -> bool:
+       '''
+       the count matters here, since we need to verify anagrams.
+       so a dictionary is needed
+       '''
+       countDict1: dict = {}
+       for char in s:
+           if not countDict1.get(char):
+               countDict1[char] = 1
+           else:    #else just increment
+               countDict1[char] = countDict1[char] + 1
+
+       countDict2: dict = {}
+
+       for char in t:
+           if not countDict2.get(char):
+               countDict2[char] = 1
+           else:    #else just increment
+               countDict2[char] = countDict2[char] + 1
+
+       #finally, we will just compare the counts
+       return countDict1 == countDict2
+
+
+
+sol = Solution()
+print(sol.isAnagram('racecar','carrace')) #True
+print(sol.isAnagram('jar','jam'))  #False
+
+
+'''
+MINIMALIST VERSION
+```python
+    countDict1[char] = countDict1.get(char,0) + 1
+```
+
+
+ALT USING Counter module (outputs a dictionary of counts of each char)
+```python
+    from collections import Counter
+    class Solution:
+        def isAnagram(self, s, t)->bool:
+            return Counter(s) == Counter(t)
+```
+'''
